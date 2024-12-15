d651dc2c25ccac5696a0a7a432faa0a345db3196
Javier <javierherreraherssens@gmail.com>
2024-12-15 19:05:19 +0100

    first commit

---
+++ scrpt.js
@@ -0,0 +1,11 @@
+// Funcionalidad para un scroll suave en el navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
